de45bfa74b251a42011087ee0c78ba1e7695fb90
kasir <kasir@DESKTOP-LQT4E5I.mshome.net>
2022-12-12 11:42:41 +0530

    second commit

---
+++ first/src/first/data.java
@@ -0,0 +1,43 @@
+package first;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class data {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					data window = new data();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public data() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
